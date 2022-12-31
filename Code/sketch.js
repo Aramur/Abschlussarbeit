71c5893b11ebb517c2afc99bb0948ccdd3804621
Aramur <benjaminnater08@gmail.com>
2022-12-31 15:07:57 +0100

    spawn with TESS shape squares

--- Code/sketch.js
+++ Code/sketch.js
@@ -4,14 +4,18 @@ function setup() {
 }
 
 var idCounter = 0;
-var x;
-var y;
-var points = [];
+var x1;
+var y1;
+var x2;
+var y2;
+var squares = [];
 
-points.push({
+squares.push({
   id: idCounter,
-  x: 0,
-  y: 600,
+  x1: 0,
+  y1: 0,
+  x2: 0,
+  y2: 600,
   delete: false
 })
 
@@ -20,29 +24,33 @@ function getRandomInt(min, max) {
 }
 
 function spawn() {
-  x = points[idCounter].x + getRandomInt(25, 100);
-  y = points[idCounter].y + getRandomInt(-50, 50);
+  x1 = squares[idCounter].x2;
+  y1 = squares[idCounter].y2;
+  x2 = x1 + getRandomInt(25, 100);
+  y2 = y1 + getRandomInt(-25, 50);
   idCounter += 1;
-  points.push({
+  squares.push({
     id: idCounter,
-    x: x,
-    y: y,
+    x1: x1,
+    y1: y1,
+    x2: x2,
+    y2: y2,
     delete: false
   })
-  console.log(x, y)
 }
 
 function draw() {
   background(220);
   spawn();
-  fill('red');
-  beginShape(TESS);
   for (var i = 0; i < idCounter; i++) {
-    vertex(points[i].x, points[i].y)
+    fill('red');
+    beginShape(TESS);
+    vertex(squares[i].x1, squares[i].y1)
+    vertex(squares[i].x2, squares[i].y2)
+    vertex(squares[i].x2, displayHeight)
+    vertex(squares[i].x1, displayHeight)
+    endShape(CLOSE);
   }
-  vertex(displayWidth, displayHeight)
-  vertex(0, displayHeight)
-  endShape(CLOSE);
 }
 
 
