6321db96852de4a5baaa0948e99a282ee46af08f
Aramur <benjaminnater08@gmail.com>
2023-03-16 15:07:59 +0100

    layerTopBoarder clean

--- Code/sketches/StoneManager.js
+++ Code/sketches/StoneManager.js
@@ -4,35 +4,69 @@ class StoneManager {
     layerY;
     layerWidth;
     layerHeight;
+    layerHeightTopBoarder;
+    layerHeightBottomBoarder;
+    biggestY;
     let = true;
 
     letStones(timeBetweenDraw) {
-        if (Helper.getRandomInt(1, timeBetweenDraw / 15) == 1) {
+        if (Helper.getRandomInt(1, timeBetweenDraw / 5) == 1) {
             this.layerWidth = Helper.getRandomInt(height / 10, height / 5);
             this.layerHeight = Helper.getRandomInt(height / 8, height / 4);
-            this.layerX = width;
-            this.layerY = Helper.getRandomInt(0, height + 500);
+            this.layerX = width + 200;
+
+            var filteredChasms = chasmManager.chasms.filter(chasm => chasm.x2 >= this.layerX);
+            filteredChasms = filteredChasms.filter(chasm => chasm.x1 <= this.layerX);
+
+            if (filteredChasms.length >= 1) {
+                var chasmWidth = filteredChasms[0].x2 - filteredChasms[0].x1;
+                var chasmHeight = filteredChasms[0].y2 - filteredChasms[0].y1;
+                var littleWidth = this.layerX - filteredChasms[0].x1;
+                var chasmPoint0 = filteredChasms[0].y1 + littleWidth / chasmWidth * chasmHeight;
+            }
 
             var filteredChasms = chasmManager.chasms.filter(chasm => chasm.x2 >= this.layerX);
             filteredChasms = filteredChasms.filter(chasm => chasm.x1 <= this.layerX + this.layerWidth);
 
+            var chasmPoint1 = 0;
             for (let chasm = 0; chasm < filteredChasms.length; chasm++) {
-                if (filteredChasms[chasm].x1 <= filteredChasms[chasm].x2) {
-                    if (this.layerY <= filteredChasms[chasm].y2) {
-                        this.let = false;
+                if (filteredChasms[chasm].x1 >= this.layerX) {
+                    if (filteredChasms[chasm].y1 <= chasmPoint1) {
+                        chasmPoint1 = filteredChasms[chasm].y1;
                     }
                 }
-                else if (this.layerY <= filteredChasms[chasm].y1) {
-                    this.let = false;
+                if (filteredChasms[chasm].x2 <= this.layerX + this.layerWidth) {
+                    if (filteredChasms[chasm].y2 >= chasmPoint1) {
+                        chasmPoint1 = filteredChasms[chasm].y1;
+                    }
                 }
             }
+
+            var filteredChasms = chasmManager.chasms.filter(chasm => chasm.x2 >= this.layerX + this.layerWidth);
+            filteredChasms = filteredChasms.filter(chasm => chasm.x1 <= this.layerX + this.layerWidth);
+
+
+            if (filteredChasms.length >= 1) {
+                var chasmWidth = filteredChasms[0].x2 - filteredChasms[0].x1;
+                var chasmHeight = filteredChasms[0].y2 - filteredChasms[0].y1;
+                var littleWidth = this.layerX - filteredChasms[0].x1;
+                var chasmPoint2 = filteredChasms[0].y1 + littleWidth / chasmWidth * chasmHeight;
+            }
+            else {
+                this.let = false;
+            }
+
+            this.layerHeightTopBoarder = max(chasmPoint0, chasmPoint1, chasmPoint2);
+
+            this.layerY = Helper.getRandomInt(this.layerHeightTopBoarder, height);
+
 
             var filteredForest = firManager.firs[0].filter(fir => fir.x + height / 4 >= this.layerX);
             filteredForest = filteredForest.filter(fir => fir.x <= this.layerX + this.layerWidth);
 
 
             for (let fir = 0; fir < filteredForest.length; fir++) {
-                if (filteredForest[fir].y + 550 / 5120 * height <= this.layerY + this.layerHeight) {
+                if (filteredForest[fir].y + 400 / 2560 * height <= this.layerY + this.layerHeight) {
                     this.let = false;
                 }
             }
